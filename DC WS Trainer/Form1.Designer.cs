656ca41ea4c0f09e4294d016f2a5010524f814f0
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2021-03-27 22:26:49 +0200

    Add project files.

---
+++ DC WS Trainer/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DC_Trainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProcessID = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.godMode_Checkbox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.creator_Label = new System.Windows.Forms.Label();
+            this.addCells_Button = new System.Windows.Forms.Button();
+            this.activateTrainer_checkbox = new System.Windows.Forms.CheckBox();
+            this.addMoney_Button = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.status_Label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(28, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process:";
+            // 
+            // ProcessID
+            // 
+            this.ProcessID.AutoSize = true;
+            this.ProcessID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProcessID.ForeColor = System.Drawing.Color.SteelBlue;
+            this.ProcessID.Location = new System.Drawing.Point(126, 207);
+            this.ProcessID.Name = "ProcessID";
+            this.ProcessID.Size = new System.Drawing.Size(103, 25);
+            this.ProcessID.TabIndex = 1;
+            this.ProcessID.Text = "Not Found";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // godMode_Checkbox
+            // 
+            this.godMode_Checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.godMode_Checkbox.ForeColor = System.Drawing.Color.SteelBlue;
+            this.godMode_Checkbox.Location = new System.Drawing.Point(33, 297);
+            this.godMode_Checkbox.Name = "godMode_Checkbox";
+            this.godMode_Checkbox.Size = new System.Drawing.Size(180, 28);
+            this.godMode_Checkbox.TabIndex = 2;
+            this.godMode_Checkbox.Text = "God Mode (F2)";
+            this.godMode_Checkbox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(77, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Add 10 Cells";
+            // 
+            // creator_Label
+            // 
+            this.creator_Label.AutoSize = true;
+            this.creator_Label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.creator_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.creator_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.creator_Label.Location = new System.Drawing.Point(184, 509);
+            this.creator_Label.Name = "creator_Label";
+            this.creator_Label.Size = new System.Drawing.Size(98, 13);
+            this.creator_Label.TabIndex = 14;
+            this.creator_Label.Text = "Created by Felix";
+            this.creator_Label.Click += new System.EventHandler(this.creator_Label_Click);
+            // 
+            // addCells_Button
+            // 
+            this.addCells_Button.BackColor = System.Drawing.Color.SteelBlue;
+            this.addCells_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addCells_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCells_Button.ForeColor = System.Drawing.Color.White;
+            this.addCells_Button.Location = new System.Drawing.Point(28, 340);
+            this.addCells_Button.Name = "addCells_Button";
+            this.addCells_Button.Size = new System.Drawing.Size(43, 35);
+            this.addCells_Button.TabIndex = 15;
+            this.addCells_Button.Text = "F3";
+            this.addCells_Button.UseVisualStyleBackColor = false;
+            // 
+            // activateTrainer_checkbox
+            // 
+            this.activateTrainer_checkbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activateTrainer_checkbox.ForeColor = System.Drawing.Color.SteelBlue;
+            this.activateTrainer_checkbox.Location = new System.Drawing.Point(33, 263);
+            this.activateTrainer_checkbox.Name = "activateTrainer_checkbox";
+            this.activateTrainer_checkbox.Size = new System.Drawing.Size(236, 28);
+            this.activateTrainer_checkbox.TabIndex = 18;
+            this.activateTrainer_checkbox.Text = "Activate Trainer (F1)";
+            this.activateTrainer_checkbox.UseVisualStyleBackColor = true;
+            this.activateTrainer_checkbox.CheckedChanged += new System.EventHandler(this.activateTrainer_checkbox_CheckedChanged);
+            // 
+            // addMoney_Button
+            // 
+            this.addMoney_Button.BackColor = System.Drawing.Color.SteelBlue;
+            this.addMoney_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addMoney_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addMoney_Button.ForeColor = System.Drawing.Color.White;
+            this.addMoney_Button.Location = new System.Drawing.Point(28, 392);
+            this.addMoney_Button.Name = "addMoney_Button";
+            this.addMoney_Button.Size = new System.Drawing.Size(43, 35);
+            this.addMoney_Button.TabIndex = 20;
+            this.addMoney_Button.Text = "F4";
+            this.addMoney_Button.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(77, 396);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 25);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Add 10000 Money";
+            // 
+            // status_Label
+            // 
+            this.status_Label.AutoSize = true;
+            this.status_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.status_Label.Location = new System.Drawing.Point(30, 509);
+            this.status_Label.Name = "status_Label";
+            this.status_Label.Size = new System.Drawing.Size(111, 13);
+            this.status_Label.TabIndex = 21;
+            this.status_Label.Text = "Status: Not Active";
+            this.status_Label.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label5.Location = new System.Drawing.Point(370, 508);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Icon by sirleviatan";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Crimson;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label4.Location = new System.Drawing.Point(30, 442);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(404, 26);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Cells and Money won\'t update the UI until their ingame value changes\r\nbut you can" +
+    " still use their new added value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackgroundImage = global::DC_Trainer.Properties.Resources.dead_cells___button_fin_1560125633132;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(487, 530);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.status_Label);
+            this.Controls.Add(this.addMoney_Button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.activateTrainer_checkbox);
+            this.Controls.Add(this.addCells_Button);
+            this.Controls.Add(this.creator_Label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.godMode_Checkbox);
+            this.Controls.Add(this.ProcessID);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dead Cells Windows Edition Trainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ProcessID;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox godMode_Checkbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label creator_Label;
+        private System.Windows.Forms.Button addCells_Button;
+        private System.Windows.Forms.CheckBox activateTrainer_checkbox;
+        private System.Windows.Forms.Button addMoney_Button;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label status_Label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DC WS Trainer/Form1.cs
@@ -0,0 +1,181 @@
+﻿using Memory;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DC_Trainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        public string codeFile = Application.StartupPath + @"\codes.ini";
+        public Mem MemLib = new Mem();
+        private int currentGameId;
+        private bool isGameLoaded;
+
+        private void OpenGame()
+        {
+            var gameName = MemLib.LoadCode("gameName", codeFile);
+            int gameProcId = MemLib.GetProcIdFromName(gameName); //use task manager to find game name. For CoD MW2 it is iw4sp. Do not add .exe extension
+
+            if (!isGameLoaded || currentGameId != gameProcId)
+            {
+                ProcessID.Invoke(new MethodInvoker(delegate { ProcessID.Text = gameProcId.ToString(); }));
+                MemLib.OpenProcess(gameProcId);
+                currentGameId = gameProcId;
+                isGameLoaded = true;
+            }
+            if (isGameLoaded && currentGameId != gameProcId)
+            {
+                isGameLoaded = false;
+                ProcessID.Invoke(new MethodInvoker(delegate { ProcessID.Text = "Not Found"; }));
+                activateTrainer_checkbox.Invoke(new MethodInvoker(delegate { activateTrainer_checkbox.Checked = false; }));
+            }
+        }
+
+        protected override void WndProc(ref Message m) //hotbuttons
+        {
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+                if (isGameLoaded && id == 1)
+                {
+                    activateTrainer_checkbox.Checked = !activateTrainer_checkbox.Checked;
+                }
+                else if (id == 2)
+                {
+                    godMode_Checkbox.Checked = !godMode_Checkbox.Checked;
+                }
+                else if (id == 3)
+                {
+                    AddCells();
+                }
+                else if (id == 4)
+                {
+                    AddMoney();
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey(this.Handle, 1, 0, (int)Keys.F1);
+            RegisterHotKey(this.Handle, 2, 0, (int)Keys.F2);
+            RegisterHotKey(this.Handle, 3, 0, (int)Keys.F3);
+            RegisterHotKey(this.Handle, 4, 0, (int)Keys.F4);
+
+            //start our backgroundworker. (similar to a timer, but works on a seperate thread)
+            if (backgroundWorker1.IsBusy == false)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true) //infinite loop
+            {
+                OpenGame();
+                //because we are working on a seperate thread from the UI, we need to use invoke on our UI elements.
+                //start reading our values
+                if (activateTrainer_checkbox.Checked)
+                {
+                    if (godMode_Checkbox.Checked)
+                    {
+                        var currentHealth = MemLib.ReadInt("currentHealth", codeFile);
+
+                        if (currentHealth > 0)
+                        {
+                            var totalHealth = MemLib.ReadInt("totalHealth", codeFile);
+                            if (totalHealth < 20000)
+                            {
+                                MemLib.WriteMemory(MemLib.LoadCode("totalHealth", codeFile), "int", "20000");
+                            }
+                            if (currentHealth < totalHealth)
+                            {
+                                MemLib.WriteMemory(MemLib.LoadCode("currentHealth", codeFile), "int", "20000");
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(900);
+            }
+        }
+
+        private void activateTrainer_checkbox_CheckedChanged(object sender, EventArgs e)
+        {
+            status_Label.Text = activateTrainer_checkbox.Checked ? "Status: Active" : "Status: Not Active";
+            if (!activateTrainer_checkbox.Checked)
+            {
+                godMode_Checkbox.Checked = false;
+            }
+        }
+
+        private void AddCells()
+        {
+            if (activateTrainer_checkbox.Checked)
+            {
+                var cells = MemLib.ReadInt("cells", codeFile);
+                cells += 10;
+                MemLib.WriteMemory(MemLib.LoadCode("cells", codeFile), "int", cells.ToString());
+            }
+        }
+
+        private void AddMoney()
+        {
+            if (activateTrainer_checkbox.Checked)
+            {
+                var money = MemLib.ReadInt("money", codeFile);
+                money += 10000;
+                MemLib.WriteMemory(MemLib.LoadCode("money", codeFile), "int", money.ToString());
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var formGraphics = e.Graphics;
+            string drawString1 = "Windows Store Edition";
+            System.Drawing.Font drawFont1 = new System.Drawing.Font("Microsoft Sans Serif", 22, System.Drawing.FontStyle.Bold);
+            System.Drawing.SolidBrush drawBrush1 = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            float x1 = 34.0F;
+            float y1 = 148.0F;
+            System.Drawing.StringFormat drawFormat1 = new System.Drawing.StringFormat();
+            formGraphics.DrawString(drawString1, drawFont1, drawBrush1, x1, y1, drawFormat1);
+
+            //377; 171
+            string drawString2 = "V22 21-01-2021";
+            System.Drawing.Font drawFont2 = new System.Drawing.Font("Microsoft Sans Serif", 8, System.Drawing.FontStyle.Bold);
+            System.Drawing.SolidBrush drawBrush2 = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            float x2 = 358.0F;
+            float y2 = 164.0F;
+            System.Drawing.StringFormat drawFormat2 = new System.Drawing.StringFormat();
+            formGraphics.DrawString(drawString2, drawFont2, drawBrush2, x2, y2, drawFormat2);
+            drawFont1.Dispose();
+            drawBrush1.Dispose();
+            drawFont2.Dispose();
+            drawBrush2.Dispose();
+            formGraphics.Dispose();
+        }
+
+        private void creator_Label_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://shababco.blogspot.com/");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.deviantart.com/sirleviatan/art/Dead-Cells-Icon-HD-868510231");
+        }
+    }
+}
